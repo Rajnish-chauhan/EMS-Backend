2adae9768329da0fd6a2f6437b6067ee563476f1
Rajnish Chauhan <crajnish425@gmail.com>
2026-08-03 01:11:52 +0530

    update url

--- src/main/java/com/project/employee_management/controller/EmployeeController.java
+++ src/main/java/com/project/employee_management/controller/EmployeeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://ems.rajnishsystems.in")
 @RestController
 @RequestMapping("/api/v1/ems")
 public class EmployeeController {
